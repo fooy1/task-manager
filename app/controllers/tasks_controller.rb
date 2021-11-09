27c401b3eaeba58f06814b152e29cec909309411
Oyvind Fossum <fooy1@pm.me>
2021-11-09 15:29:27 +0100

    m

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :destroy, :update]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    # @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    end
+  end
+
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path
+    end
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    if @task.destroy
+      redirect_to tasks_path
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
